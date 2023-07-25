743e2d277db6f78e1d5b8873ae50e82908d90930
lenagy <levente.nagy@odigo.com>
2023-07-25 14:49:46 +0200

    Setup project with tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '50-missions'`, () => {
+  it(`should have as title '50 missions board game'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('50-missions');
+    expect(app.title).toEqual('50 missions board game');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('50-missions app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('50 missions board game app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = '50-missions';
+  title = '50 missions board game';
 }
